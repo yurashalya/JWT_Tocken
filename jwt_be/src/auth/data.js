f4f10a55c86a77b3cbda241c947eeb5b72e87427
yurashalya <stallker9696@gmail.com>
2022-10-16 20:01:45 +0300

    Created BE part / added logic for signature token + create API form login request

---
+++ jwt_be/src/auth/data.js
@@ -0,0 +1,11 @@
+const passwordSecret = "aqswde";
+
+const fakeUser = {
+  login: "admin",
+  passwordHash: "dbfe89fcac9b756e50c177e2ee05af999a771fd1833fdc883f26e87f0b0bb457",
+};
+
+module.exports = {
+  passwordSecret,
+  fakeUser,
+};
